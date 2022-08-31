424bea98f55edd1b4ee1890999714f38638ce183
Faris Bassam <fofowx@gmail.com>
2022-08-31 10:59:51 +0700

    Odoo 13 Select Unselect all Company

---
+++ web_select_all_company/__manifest__.py
@@ -0,0 +1,17 @@
+# © 2021 Rizka Dwitama
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+{
+	"name": 'Web Select/Unselect All Company',
+	"summary": """User Can Select/Unselect All in MultiCompany """,
+	"version": "1.0.0.0.0",
+	"author": "Faris Bassam",
+	"category": "Web",
+	"depends": [
+		'base','web'
+	],
+	"license": "LGPL-3",
+	"qweb": ["static/src/xml/unselect_all_company.xml"],
+	"data": [],
+	'installable': True,
+	'application': False,
+}
